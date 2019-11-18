4e512b624af4571ec913542fb742ef3108f06c51
Connor Novak <connorrnovak@gmail.com>
2019-11-18 09:07:38 -0500

    Work done on speeding up and visualizing

--- scripts/datasetParser.py
+++ scripts/datasetParser.py
@@ -2,6 +2,7 @@
 import rospy
 import ros_numpy
 import time
+import timeit
 
 import matplotlib.pyplot as plt
 import tensorflow as tf
@@ -35,12 +36,12 @@ class DatasetParser:
             # Sub-select points into new PointCloud2 if within marker rect
             print("Parsing marker %i" % m.id)
             t = time.time()
-            obj_pcls[m.id] = [pt for pt in data_pts if self.is_in_bbox(m, pt)]
+            obj_pcls[m.id] = [pt for pt in data_pts if self.is_in_bbox_fast(m, pt)]
             print("Took %.2f sec" % (time.time() - t))
 
         return obj_pcls
 
-    def is_in_bbox(self, bbox, point):
+    def is_in_bbox_readable(self, bbox, point):
         """Return True if point within bbox in xy-plane.
 
         Args:
@@ -87,7 +88,54 @@ class DatasetParser:
         return True if self.is_between_lines(w1, w2, point) \
             and self.is_between_lines(l1, l2, point) else False
 
-    def is_between_lines(self, l1, l2, p):
+    def is_in_bbox_fast(self, bbox, point):
+        """Return True if point within bbox in xy-plane.
+
+        Args:
+            bbox: Marker of type CUBE representing bounding box.
+            point: obj representing point to check, has x & y attr
+
+        Returns:
+            bool True if point in box else False
+        """
+        
+        # Simplify variables for some marker attributes
+        quat = bbox.pose.orientation
+        angle = euler_from_quaternion([quat.x, quat.y, quat.z, quat.w])[2]
+        cntr = bbox.pose.position
+
+        # Calculate offsets in xy-coordinates for each side
+        l = XYPair(
+            0.5 * bbox.scale.y * np.cos(np.radians(angle)),
+            0.5 * bbox.scale.y * np.sin(np.radians(angle)))
+        w = XYPair(
+            0.5 * bbox.scale.x * np.cos(np.radians(90 + angle)),
+            0.5 * bbox.scale.x * np.sin(np.radians(90 + angle)))
+
+        # Calculate corner points
+        p1 = XYPair(cntr.x + l.x + w.x, cntr.y + l.y + w.y)
+        p2 = XYPair(cntr.x - l.x - w.x, cntr.y - l.y - w.y)
+        p3 = XYPair(cntr.x + l.x - w.x, cntr.y + l.y - w.y)
+        p4 = XYPair(cntr.x - l.x + w.x, cntr.y - l.y + w.y)
+
+        # Create functions for lines representing bbox sides
+        def w1(x):
+            return ((p4.y - p2.y) / (p4.x - p2.x)) * (x - p4.x) + p4.y
+
+        def w2(x):
+            return ((p3.y - p1.y) / (p3.x - p1.x)) * (x - p3.x) + p3.y
+
+        def l1(x):
+            return ((p2.y - p3.y) / (p2.x - p3.x)) * (x - p2.x) + p2.y
+
+        def l2(x):
+            return ((p1.y - p4.y) / (p1.x - p4.x)) * (x - p1.x) + p1.y
+
+        # Check that point is between both sets of parallel lines
+        return True if self.is_between_lines_readable(w1, w2, point) \
+            and self.is_between_lines_readable(l1, l2, point) else False
+
+    def is_between_lines_readable(self, l1, l2, p):
         """Return true if point is between parallel lines.
 
         Args:
@@ -110,6 +158,30 @@ class DatasetParser:
         else:
             return True if y1 < y0 < y2 else False
 
+    def is_between_lines_fast(self, l1, l2, px, py):
+        """Return true if point is between parallel lines.
+
+        Args:
+            l1: function that returns y for any x
+            l2: function that returns y for any x
+            px: x-coord of point in question
+            py: y-coord ...
+
+        Returns:
+            bool if point between parallel lines.
+
+        """
+        if not abs(round(l1(-2) - l1(2), 3)) \
+                == abs(round(l2(-2) - l2(2), 3)):
+            raise ValueError('lines are not parallel!')
+
+        y1, y2 = l1(px), l2(px)
+        # If line 1 is above line 2
+        if y1 > y2:
+            return True if y2 < py < y1 else False
+        else:
+            return True if y1 < py < y2 else False
+
     def run(self):
         plt.ion()
         plt.show()
@@ -127,4 +199,4 @@ if __name__ == "__main__":
         frame.ParseFromString(bytearray(data.numpy()))
         pcl = conversion.convert2pcl(conversion.frame2points(frame))
         bboxes = conversion.convert2markerarray(frame.laser_labels)
-        parser.computeClusters(pcl, bboxes)
+        pcls = parser.compute_clusters(pcl, bboxes)
