ba7678dd05a7ec2b536e40851536f53053655591
Connor Novak <connorrnovak@gmail.com>
2019-11-17 23:16:13 -0500

    Break Wayno2ROS class into functional module and visualization module.

--- scripts/waymo2ros.py
+++ scripts/waymo2ros.py
@@ -24,42 +24,35 @@ from tf.transformations import quaternion_from_euler
 tf.enable_eager_execution()
 
 
-class Waymo2ROS:
+class Waymo2ROSViz:
 
     def __init__(self, frame_id="base_link"):
-        """Initialize node, publishers, state vars."""
-        self.frame_id = frame_id
-        self.label_ids = {}  # stores label - int correspondence 
-
+        """Initialize node, publishers."""
         rp.init_node("waymo2ros")
         self.pcl_pub = rp.Publisher(
             "/pcl", PointCloud2, queue_size=1)
         self.box_pub = rp.Publisher(
             "/visualization_marker_array", MarkerArray, queue_size=1)
 
+        self.converter = Waymo2ROS()
+
     def update(self, frame):
-        frame.ParseFromString(bytearray(data.numpy()))
-        frame.lasers.sort(key=lambda laser: laser.name)
-
-        (range_images, camera_projections, range_image_top_pose) = \
-            frame_utils.parse_range_image_and_camera_projection(frame)
-
-        points, cp_points = frame_utils.convert_range_image_to_point_cloud(
-            frame,
-            range_images,
-            camera_projections,
-            range_image_top_pose)
-
-        points_all = np.concatenate(points, axis=0)
-        print(points_all.shape)
-
-        pcl_msg = self.convert2pcl(points_all)
+        """Publish pointcloud and bounding boxes from given frame."""
+        points_raw = self.converter.frame2points(frame)
+        pcl_msg = self.converter.convert2pcl(points_raw)
         self.pcl_pub.publish(pcl_msg)
 
-        markerarray_msg = self.convert2markerarray(frame.laser_labels)
-        self.box_pub.publish(markerarray_msg)
+        bbox_msg = self.converter.convert2markerarray(frame.laser_labels)
+        self.box_pub.publish(bbox_msg)
 
-    def convert2pcl(self, points):
+
+class Waymo2ROS:
+
+    def __init__(self):
+        """Initialize state vars."""
+        self.label_ids = {}  # stores label - int correspondence
+
+    def convert2pcl(self, points, frame_id='base_link'):
         """Convert list of points into ros PointCloud2 msg."""
         data = np.zeros(points.shape[0], dtype=[
           ('x', np.float32),
@@ -71,31 +64,16 @@ class Waymo2ROS:
         data['z'] = points[:, 2]
 
         msg = ros_numpy.msgify(
-            PointCloud2, data, stamp=None, frame_id=self.frame_id)
+            PointCloud2, data, stamp=None, frame_id=frame_id)
         return msg
 
-    def get_label_id(self, label):
-        """Return numeric id of label from label_ids.
-
-        Checks for label in hash map. If exists, returns int.
-        Otherwise, adds label and returns newly populated consecutive id.
-        """
-        if label not in self.label_ids:
-            self.label_ids[label] = len(self.label_ids)
-        return self.label_ids[label]
-
-    def get_label_color(self, label):
-        """Return seeded random color for label."""
-        random.seed(self.get_label_id(label))
-        return [random.uniform(0, 1) for i in range(3)]
-
-    def convert2markerarray(self, labels):
+    def convert2markerarray(self, labels, frame_id='base_link'):
         """Convert list of waymo dataset labels to bounding boxes in rviz."""
         msg = MarkerArray()
 
         for i, label in enumerate(labels):
             m = Marker()
-            m.header.frame_id = self.frame_id
+            m.header.frame_id = frame_id
             # m.header.stamp = rp.get_time()
             m.ns = "waymo2ros"
 
@@ -103,7 +81,7 @@ class Waymo2ROS:
             m.id = self.get_label_id(name)
             m.type = m.CUBE
 
-            m.action = m.MODIFY if name in self.label_ids.keys() else m.ADD 
+            m.action = m.MODIFY if name in self.label_ids.keys() else m.ADD
             m.pose.position.x = label.box.center_x
             m.pose.position.y = label.box.center_y
             m.pose.position.z = label.box.center_z
@@ -123,9 +101,40 @@ class Waymo2ROS:
 
         return msg
 
+    def frame2points(self, frame):
+        """Extract points from waymo frame."""
+        frame.ParseFromString(bytearray(data.numpy()))
+        frame.lasers.sort(key=lambda laser: laser.name)
+
+        (range_images, camera_projections, range_image_top_pose) = \
+            frame_utils.parse_range_image_and_camera_projection(frame)
+
+        points, cp_points = frame_utils.convert_range_image_to_point_cloud(
+            frame,
+            range_images,
+            camera_projections,
+            range_image_top_pose)
+
+        return np.concatenate(points, axis=0)
+
+    def get_label_color(self, label):
+        """Return seeded random color for label."""
+        random.seed(self.get_label_id(label))
+        return [random.uniform(0, 1) for i in range(3)]
+
+    def get_label_id(self, label):
+        """Return numeric id of label from label_ids.
+
+        Checks for label in hash map. If exists, returns int.
+        Otherwise, adds label and returns newly populated consecutive id.
+        """
+        if label not in self.label_ids:
+            self.label_ids[label] = len(self.label_ids)
+        return self.label_ids[label]
+
 
 if __name__ == "__main__":
-    conversion = Waymo2ROS()
+    conversion = Waymo2ROSViz()
 
     # TODO: Make this more general
     FILENAME = '/home/cnovak/Workspaces/catkin_ws/src/waymo-od/tutorial/frames'
@@ -169,11 +178,11 @@ if __name__ == "__main__":
 
 
     print(frame.context)
-    
+
     plt.figure(figsize=(64, 20))
     def plot_range_image_helper(data, name, layout, vmin = 0, vmax=1, cmap='gray'):
       ""\"Plots range image.
-    
+
       Args:
         data: range image data
         name: the image title
@@ -188,14 +197,14 @@ if __name__ == "__main__":
       plt.grid(False)
       plt.axis('off')
       # plt.show()
-    
+
     def get_range_image(laser_name, return_index):
       ""\"Returns range image given a laser name and its return index.""\"
       return range_images[laser_name][return_index]
-    
+
     def show_range_image(range_image, layout_index_start = 1):
       ""\"Shows range image.
-    
+
       Args:
         range_image: the range image data from a given lidar of type MatrixFloat.
         layout_index_start: layout offset
@@ -214,19 +223,19 @@ if __name__ == "__main__":
                        [8, 1, layout_index_start + 1], vmax=1.5, cmap='gray')
       plot_range_image_helper(range_image_elongation.numpy(), 'elongation',
                        [8, 1, layout_index_start + 2], vmax=1.5, cmap='gray')
-    
-    
+
+
     frame.lasers.sort(key=lambda laser: laser.name)
     show_range_image(get_range_image(open_dataset.LaserName.TOP, 0), 1)
     show_range_image(get_range_image(open_dataset.LaserName.TOP, 1), 4)
-    
+
     #
     points, cp_points = frame_utils.convert_range_image_to_point_cloud(
         frame,
         range_images,
         camera_projections,
         range_image_top_pose)
-    
+
     points_all = np.concatenate(points, axis=0)
     print(points_all)
     """
