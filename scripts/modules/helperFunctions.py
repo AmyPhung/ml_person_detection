dd4b481c974322c95a58563dda795359fdea6ec6
Connor Novak <connorrnovak@gmail.com>
2019-11-18 22:33:11 -0500

    Layout function stubs for helper funcs

--- scripts/modules/helperFunctions.py
+++ scripts/modules/helperFunctions.py
@@ -1,3 +1,13 @@
+"""Module for shared code between waymo pipeline and panasonic pipeline.
+
+This module holds functions that are used in both pipelines:
+    remove_groundplane()
+    extract_pcl_features()
 """
-put filtering function here
-"""
+
+def remove_groundplane(pcl, ground_height):
+    """Remove points in ground plane and return pcl."""
+    pass
+
+def extract_pcl_features(pcl):
+    """Calculate features for net training from pcl, return in some format."""
