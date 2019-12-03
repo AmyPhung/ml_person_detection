cd87ed8dbb12ad109dcad85b32356ccba9f543e9
AmyPhung <amyngph@gmail.com>
2019-12-02 22:51:25 -0500

    Revise json2csv to support new features

--- scripts/modules/json2csv.py
+++ scripts/modules/json2csv.py
@@ -1,4 +1,4 @@
-#!usr/bin/env python2
+#!/usr/bin/env python2
 
 import csv
 import glob
@@ -17,8 +17,11 @@ def json2csv(json_dir, csv_dir, csv_name):
 
     dict_data = []
     fieldnames = [
-        'frame_index', 'cluster_id', 'cnt', 'cls', 'density', 'vol', 'e_x', 'e_y', 'e_z']
+        'frame_index', 'cluster_id', 'cnt', 'cls',
+        'density', 'vol', 'e_x', 'e_y', 'e_z',
+        'max_intensity', 'avg_intensity', 'var_intensity']
 
+    print('%s/*.json' % json_dir)
     # Populate dict_data from all .json files
     for f in glob.glob('%s/*.json' % json_dir):
         frame_index = int(re.search('-([0-9]+).json$', f).group(1))
@@ -31,13 +34,29 @@ def json2csv(json_dir, csv_dir, csv_name):
 
     # Write dict_data to csv
     try:
+        print('%s/%s.csv' % (csv_dir, csv_name))
         with open('%s/%s.csv' % (csv_dir, csv_name), 'w') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
             for data in dict_data:
-                writer.writerow(data)
+                data_row = {}
+                data_row['frame_index'] = data['frame_index']
+                data_row['cluster_id'] = data['cluster_id']
+                data_row['cnt'] = data['cnt']
+                data_row['cls'] = data['cls']
+                data_row['density'] = data['parameters'][0]
+                data_row['vol'] = data['parameters'][1]
+                data_row['e_x'] = data['parameters'][2]
+                data_row['e_y'] = data['parameters'][3]
+                data_row['e_z'] = data['parameters'][4]
+                data_row['max_intensity'] = data['parameters'][5]
+                data_row['avg_intensity'] = data['parameters'][6]
+                data_row['var_intensity'] = data['parameters'][7]
+                # print(data_row)
+                writer.writerow(data_row)
+
     except IOError:
-        print("I/O error") 
+        print("I/O error")
 
 if __name__ == "__main__":
 
